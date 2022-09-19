1a69a60077fe286ddd6c393884243c0d49499412
mylest913 <mylest913@gmail.com>
2022-09-18 22:11:30 -0700

    Getting started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Routes, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Menu from "./components/Menu";
+import "./components/style.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="nav">
+        <h1 className="name">Lisha's Pizza</h1>
+        <ul>
+          <li>
+            <Link className="links" to="/">
+              <h4>Home</h4>
+            </Link>
+          </li>
+          <li>
+            <Link className="links" to="/menu">
+              <h4>Menu</h4>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/components/Menu.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+// import "./components/style.css";
+import pizza from "../pizza";
+const Menu = () => {
+  return <div>MENU</div>;
+};
+
+export default Menu;
